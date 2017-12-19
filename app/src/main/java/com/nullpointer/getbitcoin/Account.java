7077dd9ded822b068eb43576b4d0f4f1cdef93b3
Kirill Khaustov <k.khaustov@evotor.ru>
2017-12-19 19:40:55 +0300

    Advertising manager implementation

---
+++ app/src/main/java/com/nullpointer/getbitcoin/Account.java
@@ -0,0 +1,25 @@
+package com.nullpointer.getbitcoin;
+
+import java.math.BigInteger;
+
+/**
+ * Created by Khaustov on 19.12.17.
+ */
+
+public class Account {
+    private final String accountUUID;
+    private final BigInteger currencyBalance;
+
+    public Account(String accountUUID, BigInteger currentBalance) {
+        this.accountUUID = accountUUID;
+        this.currencyBalance = currentBalance;
+    }
+
+    public BigInteger getCurrencyBalance() {
+        return currencyBalance;
+    }
+
+    public String getAccountUUID() {
+        return accountUUID;
+    }
+}
