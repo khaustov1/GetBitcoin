8674844ec372328aedcc4250a8196124499d763e
Kirill Khaustov <k.khaustov@evotor.ru>
2017-12-19 15:25:13 +0300

    Advertising manager implementation

--- app/src/main/java/com/nullpointer/getbitcoin/TapJoyManager.java
+++ app/src/main/java/com/nullpointer/getbitcoin/TapJoyManager.java
@@ -1,17 +1,38 @@
 package com.nullpointer.getbitcoin;
 
+import android.util.Log;
+
 import com.tapjoy.TJConnectListener;
+import com.tapjoy.TJGetCurrencyBalanceListener;
 import com.tapjoy.Tapjoy;
 
 import java.lang.ref.WeakReference;
 import java.util.Hashtable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Khaustov on 19.12.17.
  */
 
 public class TapJoyManager implements TJConnectListener {
+    private static final int GET_BALANCE_INTERVAL = 30;
+    private static final int GET_BALANCE_START_DELAY = 10;
+
     private final WeakReference<MainActivity> contextWeakReference;
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private final TJGetCurrencyBalanceListener balanceListener = new TJGetCurrencyBalanceListener() {
+        @Override
+        public void onGetCurrencyBalanceResponse(String currencyName, int balance) {
+            Log.i("Tapjoy", "getCurrencyBalance returned " + currencyName + ":" + balance);
+        }
+
+        @Override
+        public void onGetCurrencyBalanceResponseFailure(String error) {
+            Log.i("Tapjoy", "getCurrencyBalance error: " + error);
+        }
+    };
 
     public TapJoyManager(MainActivity mainActivity) {
         contextWeakReference = new WeakReference<>(mainActivity);
@@ -20,11 +41,18 @@ public class TapJoyManager implements TJConnectListener {
     public void initialize() {
         if (contextWeakReference.get() != null) {
             Tapjoy.connect(contextWeakReference.get(),
-                    contextWeakReference.get().getString(R.string.api_key), new Hashtable(), this);
+                    contextWeakReference.get().getString(R.string.api_key),
+                    new Hashtable(), this);
             Tapjoy.setDebugEnabled(true);
         } else {
             throw new RuntimeException("Main activity is dead");
         }
+        executorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                Tapjoy.getCurrencyBalance(balanceListener);
+            }
+        }, GET_BALANCE_START_DELAY, GET_BALANCE_INTERVAL, TimeUnit.SECONDS);
     }
 
     public void start() {
@@ -41,11 +69,11 @@ public class TapJoyManager implements TJConnectListener {
 
     @Override
     public void onConnectSuccess() {
-
+        Log.d("Tapjoy", "Connection established successfully");
     }
 
     @Override
     public void onConnectFailure() {
-
+        Log.e("Tapjoy", "Connection failed");
     }
 }
